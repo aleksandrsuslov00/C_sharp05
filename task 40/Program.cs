e2c08489d6c94200d4c55283afc4f2a91f6ac855
Aleksandr <a.suslov631@gmail.com>
2023-07-13 20:27:35 +0200

    task 40 (triangleExist?) complete

---
+++ task 40/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 40.
+Напишите программу, которая принимает 3 числа 
+и проверяет может ли сущетсвовать треугольник 
+со сторонами такой длины.
+*/
+
+bool IsTriangleExist (int sideA, int sideB, int sideC)
+{
+    if ((sideA + sideB > sideC) 
+    && (sideB + sideC > sideA) 
+    && (sideA + sideC > sideB))
+        return true;
+    return false;
+}
+Console.WriteLine("Укажите сторону А");
+int sideA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите сторону B");
+int sideB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите сторону C");
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+if (IsTriangleExist(sideA, sideB, sideC))
+{
+    Console.WriteLine("Треугольник существует");
+} 
+else Console.WriteLine("Треугольник несуществует");
+
+
