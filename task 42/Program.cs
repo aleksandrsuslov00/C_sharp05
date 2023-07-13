65949b886fcaa5ddc53826a82bd9cadb206da333
Aleksandr <a.suslov631@gmail.com>
2023-07-14 00:59:05 +0200

    task 42 complete

---
+++ task 42/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 42.
+Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+*/
+
+Console.WriteLine("Введите число ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int binary = 0;
+int shift = 1; //сдвигает результаты
+while(value != 0)
+{
+    binary += value % 2 * shift; // или binary = binary + value / 2 * shift;
+    shift *= 10;
+    value /= 2;
+}
+
+Console.WriteLine(binary);
