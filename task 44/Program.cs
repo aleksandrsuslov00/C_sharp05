c73df29cd5e0cc4863c0deec57f28025124c163d
Aleksandr <a.suslov631@gmail.com>
2023-07-14 01:43:06 +0200

    task 44 fibonachchi complete

---
+++ task 44/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 44.
+Неиспользуя рекурсию, выведете первые N чисел Фибоначчи. 
+Первые два числа 0 и 1.
+*/
+Console.Clear();
+Console.WriteLine("введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int number1 = 0;
+int number2 = 1;
+
+int fibi = 0;
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.Write(number1 + " ");
+Console.Write(number2 + " ");
+
+for (int i = 2; i < numberN; i++)
+{
+    fibi = number1 + number2;
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(fibi + " ");
+    number1 = number2;
+    number2 = fibi;
+}
+Console.ForegroundColor = ConsoleColor.White;
