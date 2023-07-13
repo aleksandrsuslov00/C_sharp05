b963b8081ba7db8e19f4d905eb937c1697f3f65b
Aleksandr <a.suslov631@gmail.com>
2023-07-13 20:02:01 +0200

    task 39 (reverse array)  with colors complete

--- task 39/Program.cs
+++ task 39/Program.cs
@@ -3,3 +3,65 @@
 Напишите программу, которая перевернет одномерный массив.
 */
 
+//метод генерации массива с рандомными числами(элементами)
+int[] GenerateArray(int lenght, int start, int end)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(start, end + 1); 
+    }
+    return array;
+}
+
+//метод вывода массива в терминал
+void PrintArray (int[] array, string name = "")
+{
+    Console.WriteLine(name);
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]"); 
+}
+
+
+//метод перевернутого массива
+int[] reverseArray (int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[array.Length - 1 - i];
+    }
+    return result;
+}
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkCyan; 
+int[] array = GenerateArray(5, -100, 100);
+PrintArray(array, "Начальный массив");
+Console.ForegroundColor = ConsoleColor.White; 
+Console.WriteLine("==========================");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+int[] arrayRev = reverseArray(array);
+PrintArray(arrayRev, "Перевёрнутый массив");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
