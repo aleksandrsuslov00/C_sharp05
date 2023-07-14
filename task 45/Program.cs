adc84845954c75a12959e513610b137df80b8a9f
Aleksandr <a.suslov631@gmail.com>
2023-07-14 02:03:59 +0200

    task 45 copyArray complete

---
+++ task 45/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 45.
+*/
+
+// копируем массив
+int[] copyArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copyArray[i] = array[i];
+    }
+    return copyArray;
+}
+
+
+//принт массива
+void printArray (int[] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i != arrayToPrint.Length - 1)
+        {
+            Console.Write(", "); // добавляем запятую между элементами до конца массива
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] test = {1, 2, 3, 4, 5};
+printArray(test);
+int[] testCopy = copyArray(test);
+printArray(testCopy);
