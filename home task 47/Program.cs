90acc9439b9566e4cf60327ca21f45cb41365b2d
Aleksandr <a.suslov631@gmail.com>
2023-07-18 14:48:23 +0200

    home task 47 complete

---
+++ home task 47/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задание 47.
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.Clear();
+
+double[,] GetRandom2DArray (int rowNumber, int colNumber)
+{
+    double[,] array = new double[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+    return array;
+}
+
+
+void Print2DArray(double[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+       Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+       Console.Write($"[{i}]\t");
+       for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+       {
+           Console.Write(arrayToPrint[i,j] + "\t");
+       }
+       Console.WriteLine();
+    }
+}
+
+double[,] randomArray = GetRandom2DArray (10, 10);
+Print2DArray(randomArray);
