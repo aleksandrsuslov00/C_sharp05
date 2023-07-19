51984bcd6a7e0860a275a0143b946add657ba625
Aleksandr <a.suslov631@gmail.com>
2023-07-20 01:23:01 +0200

    home task 52 complete

--- home task 52/Program.cs
+++ home task 52/Program.cs
@@ -1,5 +1,4 @@
 ﻿/*
-
 Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 Например, задан массив:
@@ -7,55 +6,66 @@
 5 9 2 3
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
 */
 
-Console.WriteLine("введите количество строк");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите количество столбцов");
-int m = Convert.ToInt32(Console.ReadLine());
-
-int[,] numbers = new int[n, m];
-FillArrayRandomNumbers(numbers);
-
-
-for (int j = 0; j < numbers.GetLength(1); j++)
+Console.Clear();
+int[,] FillArray(int rowNumber, int colNumber)
 {
-    double avarage = 0;
-    for (int i = 0; i < numbers.GetLength(0); i++)
+    int[,] array = new int [rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
     {
-        avarage = (avarage + numbers[i, j]);
-    }
-    avarage = avarage / n;
-    Console.Write(avarage + "; ");
-}
-Console.WriteLine();
-PrintArray(numbers);
-
-
-
-void FillArrayRandomNumbers(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < colNumber; j++)
         {
             array[i, j] = new Random().Next(0, 10);
         }
     }
+    return array;
 }
 
-void PrintArray(int[,] array)
+void PrintArray(int[,] arrayToPrint)
 {
-
-    for (int i = 0; i < array.GetLength(0); i++)
+    Console.Write($"[ ]\t");
+    
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
     {
-        Console.Write("[ ");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.Write("]");
-        Console.WriteLine("");
+       Console.Write($"[{i}]\t");
+    }
+    
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+       Console.Write($"[{i}]\t");
+       for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+       {
+           Console.Write(arrayToPrint[i,j] + "\t");
+       }
+       Console.WriteLine();
     }
 }
+
+Console.WriteLine("Выберите количество строк: ");
+int rowNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Выберите количество столбцов: ");
+int colNumber = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+int[,] numbers = FillArray(rowNumber, colNumber);
+PrintArray(numbers);
+Console.Write("Среднее арифметическое каждого столбца равно: ");
+
+void averageCol(int[,] arrayToPrint)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+        {
+            average = (average + arrayToPrint[i, j]); 
+        }
+        average = average / rowNumber;
+        Console.Write($"{average}; ");
+    }
+}
+
+averageCol(numbers);
+
