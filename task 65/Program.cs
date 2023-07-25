7c00cbc569d7d17abedf9850281d9f2d95bf0939
Aleksandr <a.suslov631@gmail.com>
2023-07-25 16:56:53 +0200

    task 65 complete

---
+++ task 65/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задайте значения М и N 
+Напишите программу которая
+выведет все натуральные числа от М до N
+
+M = 1; N = 5 -> "1,2,3,4,5"
+M = 4; N = 8 -> "4,5,6,7,8"
+*/
+Console.WriteLine("Введите число N: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int end = Convert.ToInt32(Console.ReadLine());
+
+void RecursionNumberNM(int start, int end)
+{
+    if (start > 1 && end > 1)
+    {
+        if (start <= end)
+        {
+            Console.Write(start + " ");
+         RecursionNumberNM(start + 1, end);
+        }
+    }
+}
+
+RecursionNumberNM(start, end);
