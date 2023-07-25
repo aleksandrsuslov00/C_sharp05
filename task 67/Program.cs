26188b248052c7dc44b19d040cdb640fc788f173
Aleksandr <a.suslov631@gmail.com>
2023-07-25 17:31:06 +0200

    task 67 complete

---
+++ task 67/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу,  которая будет принимать на вход число и возвращать сумму его цифр.
+*/
+Console.WriteLine("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int recSum(int num)
+{
+    if(num != 0)
+    {
+        return num %  10 + recSum(num/10);
+    }
+    return 0;
+}
+
+Console.WriteLine(recSum(num));
