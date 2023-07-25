c9c90be69419049ca05ebc1648ff6b32b839beb5
Aleksandr <a.suslov631@gmail.com>
2023-07-25 18:03:43 +0200

    task 69 complete

---
+++ task 69/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+напишите программу которая на вход принимает А и Б
+и возводит А в целую степень Б с помощью рекрсии
+*/
+
+int AToPowB(int a, int b)
+{
+    if (b == 1)
+    {
+        return a;
+    }
+    else
+    {
+        return a * AToPowB(a, b - 1);
+    }
+}
+Console.WriteLine(AToPowB(2, 9));
